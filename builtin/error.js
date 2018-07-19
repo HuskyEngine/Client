6a819f3a71ece5fabf0b71943cfebbdc2ffa16b5
Keith Armstrong <keith1015@gmail.com>
2018-07-19 08:23:21 -0500

    Fix error page formatting

--- builtin/error.js
+++ builtin/error.js
@@ -38,6 +38,6 @@ game.scripts.render = (frame) => {
   fillStyle("red", L_GAME);
   fillRect(0, 0, 100, 100, L_GAME);
   fillStyle("white", L_GAME);
-  text("Husky Engine encountered an error: " + game.loadArgs.type, "32pt Arial", 4.883, 8.681, L_GAME);
-  text(game.loadArgs.msg, "32pt Arial", 6.104, 13.021, L_GAME);
+  text("Husky Engine encountered an error: " + game.loadArgs.type, {size: 20, font: "Arial"}, 4, 8, L_GAME);
+  text(game.loadArgs.msg, {size: 18, font: "Arial"}, 6, 12, L_GAME);
 };
